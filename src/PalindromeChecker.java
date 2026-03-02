1069130ca6fd7542fe853f5efa869bf2528a94ea
Chetna-191 <mohantychetna191@gmail.com>
2026-03-02 16:25:40 +0530

    UC12 :Strategy Pattern for Palindrome Algorithms

--- src/PalindromeChecker.java
+++ src/PalindromeChecker.java
@@ -1,24 +1,39 @@
 import java.util.Scanner;
 
 public class PalindromeChecker {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Enter Input : ");
-        String input = sc.nextLine();
-        String normalized = input.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
-        boolean isPalindrome = true;
-        for (int i = 0; i < normalized.length() / 2; i++) {
 
-            if (normalized.charAt(i) !=
-                    normalized.charAt(normalized.length() - 1 - i)) {
-                isPalindrome = false;
-                break;
-            }
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("=== Palindrome Checker App (UC12 - Strategy Pattern) ===");
+        System.out.print("Enter a string: ");
+        String input = scanner.nextLine();
+
+        System.out.println("Choose Algorithm:");
+        System.out.println("1. Stack Strategy");
+        System.out.println("2. Deque Strategy");
+        System.out.print("Enter choice (1 or 2): ");
+        int choice = scanner.nextInt();
+
+        PalindromeStrategy strategy;
+
+        if (choice == 1) {
+            strategy = new StackStrategy();
+        } else {
+            strategy = new DequeStrategy();
         }
 
-        System.out.println("Input : " + input);
-        System.out.println("Is Palindrome? : " + isPalindrome);
+        PalindromeContext context = new PalindromeContext(strategy);
 
-        sc.close();
+        boolean result = context.execute(input);
+
+        if (result) {
+            System.out.println("Result: The given string is a Palindrome.");
+        } else {
+            System.out.println("Result: The given string is NOT a Palindrome.");
+        }
+
+        scanner.close();
     }
 }
